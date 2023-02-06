c8c4f93f4d6686ceac61da218f99c1966957e6bf
guoyiyang <yiyangguo@tencent.com>
2023-02-06 16:37:57 +0800

    first commit

---
+++ pages/user/user.vue
@@ -0,0 +1,40 @@
+<template>
+
+</template>
+<script>
+	export default {
+		components: {},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+		
+		}
+	}
+</script>
+<style lang="scss">
+	.search-result {
+		padding-top: 10px;
+		padding-bottom: 20px;
+		text-align: center;
+	}
+
+	.search-result-text {
+		text-align: center;
+		font-size: 14px;
+		color: #666;
+	}
+
+	.example-body {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+		padding: 0px;
+	}
+
+	.uni-mt-10 {
+		margin-top: 10px;
+	}
+</style>
