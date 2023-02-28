0009958e7c8566dbc9fa51b06fa8801e8c8f6b9b
Guoyiyang <guo.yy@outlook.com>
2023-02-28 23:55:39 +0800

    update

--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -9933,8 +9933,8 @@ var setting = {
   // 	'vab-mock-server' :
   // 	'vab-mock-server',
   // baseUrl: 'http://192.168.1.6:8888'
-  baseUrl: 'http://localhost:8888'
-  // baseUrl: 'https://yiyangguo.xyz'
+  // baseUrl: 'http://localhost:8888',
+  baseUrl: 'https://yiyangguo.xyz'
   // baseUrl: 'http://118.195.215.159:8888',
 };
 
